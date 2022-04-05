d77b9d51501a67edaf9d4a7611d786feff4d7950
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2022-04-05 08:45:51 +0200

    🔨 Fix looking direction and talking face at end

--- Rooms/Kueche/KitchenMomArea.gd
+++ Rooms/Kueche/KitchenMomArea.gd
@@ -5,15 +5,20 @@ func action():
 	var mom = get_tree().get_nodes_in_group("mom")[0];
 	var oven = get_tree().get_nodes_in_group("oven")[0];
 	
+	mom.look_direction = Vector2.LEFT;
+	mom.get_node("AnimatedSprite").animation = "walk_side";
+	mom.get_node("AnimatedSprite").scale.x = -1;
+	
 	if (
 		Globals.ingredients_on_counter["Milk"] or 
 		Globals.ingredients_on_counter["Eggs"] or 
 		Globals.ingredients_on_counter["Flour"] or 
 		Globals.ingredients_on_counter["Bowl"]
 	):
-		yield(MessageSystem.show_message("mom", "Why did my son leave groceries on the table?", "neutral"), "done");
+		yield(MessageSystem.show_message("mom", "Why did my son leave groceries on the counter?", "neutral"), "done");
 	
 	if oven.cake_burned:
+		yield(MessageSystem.show_message("mom", "What's that horrible smell?", "mad"), "done");
 		yield(MessageSystem.show_message("mom", "THIS LITTLE BRAT! Oh, I can´t wait to get to his room!", "mad"), "done");
 
 	return .action();
--- Rooms/Zimmer/MomArea.gd
+++ Rooms/Zimmer/MomArea.gd
@@ -4,6 +4,6 @@ func action():
 	
 	yield(MessageSystem.show_message("mom", "Bob... I've got a bone to pick with you.", "mad"), "done");
 	
-	yield(MessageSystem.show_message("player", "Thank you for playing!", "mad"), "done");
+	yield(MessageSystem.show_message("bob", "Thank you for playing!", "happy"), "done");
 	
 	return .action();
